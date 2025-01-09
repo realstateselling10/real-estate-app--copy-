dca69a6ce9014a4150458f961b2cf8b22a741ba9
realstateselling10 <realstateselling10@gmail.com>
2025-01-09 22:13:43 +0545

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -30,7 +30,6 @@ const Login = () => {
 
             if (response.data.token) {
                 // Add console log to verify token received
-                console.log('Login successful, response.data.token);
                 Cookies.set('jwtCookie', response.data.token, { expires: 7, secure: true }); // Set cookie to expire in 7 days
             
                 navigate('/admin');
